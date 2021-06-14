b54b4d4e97be4ce0c10ed009a8e12cc8ad85aea3
Guilherme Sousa Ribeiro <guilherme.tec1107@gmail.com>
2021-06-14 16:11:48 -0300

    class

---
+++ atividade-20/script.js
@@ -0,0 +1,11 @@
+alert("Hello world!")
+console.log("Olá mundo")
+//Decremento --
+var a = 3
+
+console.log(a)
+document.write(a)
+
+var b = a++ //primeiro b = a DEPOIS a = a + 1
+var b = ++a //primeiro a = a + 1 DEPOIS b = a
+console.log(a)
