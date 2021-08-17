11ad561670651a3f539d95c9147e04b207bc1fc7
Guilherme Sousa Ribeiro <guilherme.tec1107@gmail.com>
2021-08-16 21:39:53 -0300

    classes

---
+++ atividade-26/script.js
@@ -0,0 +1,23 @@
+
+
+// var é global
+// let só funciona no bloco em que foi criada ou seja escopo local
+// const não pode ser reatribuida, pode ser modificada e tem escopo local
+
+
+//aparentemente, é mais fácil de se encontrar erro no código evitando usar "var"
+
+{
+var numero1 = 4
+let numero2 = 5
+const numero3 = true
+
+
+console.log(numero1)
+console.log(numero2)
+console.log(numero3)
+}
+
+let numero2 = 3
+
+console.log(numero2)
