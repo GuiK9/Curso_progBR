4f463db1600e72bb24c66b677f99677d0ff99bb4
Guilherme Sousa Ribeiro <guilherme.tec1107@gmail.com>
2021-08-28 00:38:14 -0300

    class

--- autonomos-do-curso/pagina-sorteio/script.js
+++ autonomos-do-curso/pagina-sorteio/script.js
@@ -6,25 +6,29 @@ let p = document.getElementsByTagName("p")[0]
 botao.onclick = sorteio
 
 function sorteio() {
-    let na = 0
-    if(ns.length == 10){
+  let na;
+  if (ns.length == 10) {
     window.alert("já foram selecionados todos os números")
-    } else{
-        while (ns.indexOf(na) != -1) {
-            na = Math.ceil(Math.random() * 10)
-            console.log(na)
-        }
+  } else {
+    while (ns.indexOf(na) != -1) {
+      na = Math.ceil(Math.random() * 10)
+      console.log(na)
     }
-   if(ns.indexOf(na) === -1) { 
-    h1.innerHTML = na
-    ns.push(na)
-   }
-    if (ns.length <= 11) {
-      p.innerHTML = ns
+    let f = inserirNum()
+
+    function inserirNum() {
+      if (ns.indexOf(na) === -1) {
+        h1.innerHTML = na
+        ns.push(na)
+      }
+      if (ns.length <= 11) {
+        p.innerHTML = ns
+      }
     }
-  
-  ns.sort(function(a, b) {
-    return a - b;
-    })
+  }
+
+  /*ns.sort(function (a, b) {
+    return a - b
+  })*/
   console.log(ns)
 }
