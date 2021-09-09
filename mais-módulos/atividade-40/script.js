73fecb6ad3a1e6b8a3a2d4f9ea3aba3f675e8fa9
Guilherme Sousa Ribeiro <guilherme.tec1107@gmail.com>
2021-09-09 19:52:25 -0300

    classes

---
+++ mais-módulos/atividade-40/script.js
@@ -0,0 +1,21 @@
+
+let jogador = document.getElementById("jogador")
+
+let xInicial = 0
+let yInicial = 0 
+
+
+function moverJogadorPara(x, y) {
+    let posx = x + "px"
+    let posy = y + "px"
+    jogador.style.top = posx
+    jogador.style.left = posy
+
+}
+
+function ande(){setInterval(function(){
+
+    moverJogadorPara(xInicial++, yInicial++)
+
+ }, 200)
+}
