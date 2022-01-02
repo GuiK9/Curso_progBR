1e32943c8471bdd2d1aee6711e942de9d4425d33
Guilherme Sousa Ribeiro <74858101+GuiK9@users.noreply.github.com>
2022-01-02 18:47:01 -0300

    delete usando o fetch, padrão

--- mais-modulos/atividade-93/controllers/linkController.js
+++ mais-modulos/atividade-93/controllers/linkController.js
@@ -2,42 +2,59 @@ const Link = require('../models/Link.js')
 
 const redirect = async (req, res, next) => {
     let title = req.params.title
-    try{
-        let doc = await Link.findOne({title})
-        if(doc){
+    try {
+        let doc = await Link.findOne({ title })
+        if (doc) {
             res.redirect(doc.url)
         } else {
             next()
         }
-    } catch(err) {
+    } catch (err) {
 
         res.send(err)
     }
 }
 
-const addLink = async(req, res) => {
+const addLink = async (req, res) => {
     let link = new Link(req.body)
-    
-    try{
+
+    try {
         await link.save()
         res.send("Link Adicionado com sucesso")
     } catch (err) {
         console.log(req.body)
-        res.render('index', {err, body: req.body})
+        res.render('index', { err, body: req.body })
     }
 
 }
 
 const allLinks = async (req, res) => {
-    
-    try{
+
+    try {
         let links = await Link.find({})
-        res.render('all', {links})
+        res.render('all', { links })
     } catch (err) {
         res.send(err)
     }
 
 }
 
+const deleteLink = async (req, res) => {
 
-module.exports = {redirect, addLink, allLinks}
+    let id = req.params.id
+    if (!id) {
+        id = req.body.id
+    
+    }
+
+    try {
+        await Link.findByIdAndDelete(id)
+        res.send(id)
+    } catch (err) {
+        res.status(404).send(err)
+    }
+}
+
+
+
+module.exports = { redirect, addLink, allLinks, deleteLink }
--- mais-modulos/atividade-93/routes/linkRoutes.js
+++ mais-modulos/atividade-93/routes/linkRoutes.js
@@ -1,17 +1,22 @@
-
+const methodOverride = require('method-override')
 const express = require('express')
 const router = express.Router()
 const linkController = require('../controllers/linkController')
+
+router.use(methodOverride('_method'))
 
 router.get('/all', linkController.allLinks)
 
 router.get('/:title', linkController.redirect)
 
 router.get("/", (req, res) => {
-    res.render('index', {err: false, body: {}})
+    res.render('index', { err: false, body: {} })
 })
 
-router.post('/', express.urlencoded({extended: true}), linkController.addLink)
+router.post('/', express.urlencoded({ extended: true }), linkController.addLink)
 
+router.delete('/:id', linkController.deleteLink)
+
+router.delete('/', express.urlencoded({ extended: true }), linkController.deleteLink)
 
 module.exports = router
