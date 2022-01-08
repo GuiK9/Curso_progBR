36236c0c7b105af0484e303e50eebd86a86bee9b
Guilherme Sousa Ribeiro <74858101+GuiK9@users.noreply.github.com>
2022-01-07 21:29:45 -0300

    login e register

--- mais-modulos/atividade-94/app.js
+++ mais-modulos/atividade-94/app.js
@@ -2,10 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const userRouter = require('./routes/userRouter')
+const mongoose = require('mongoose')
 
 
-app.use('/user', userRouter)
+mongoose.connect(process.env.MONGO_CONNECTION_URL, (error) => {
+    if (error)
+        console.log(error)
+    else
+        console.log("mongo connected")
+
+})
+
+app.use('/user', express.json(), userRouter)
 
 
 
-app.listen(process.env.PORT, () => {console.log('server running')}) 
+app.listen(process.env.PORT, () => { console.log('server running') }) 
--- mais-modulos/atividade-94/controllers/userController.js
+++ mais-modulos/atividade-94/controllers/userController.js
@@ -1,13 +1,38 @@
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+
 const userController = {
 
-    register: function (req, res) {
-        console.log('register')
-        res.send('register')
+    register: async function (req, res) {
+
+        const selectedUser = await User.findOne({email:req.body.email})
+
+        if(selectedUser) return res.status(400).send("email already exist")
+
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password)
+        })
+
+        try {
+            const savedUser = await user.save()
+            res.send(savedUser)
+        } catch (error) {
+            res.status(400).send(error)
+        }
     },
 
-    login: function (req, res) {
-        console.log('login')
-        res.send('login')
+    login: async function (req, res) {
+
+        const selectedUser = await User.findOne({email:req.body.email})
+        if(!selectedUser) return res.status(400).send("email or password incorrect")
+
+        const passwordAndUserMatch = bcrypt.compareSync(req.body.password, selectedUser.password)
+
+        if(!passwordAndUserMatch) return res.status(400).send("email or password incorrect")
+
+        res.send("User logged")
     }
 
 }
