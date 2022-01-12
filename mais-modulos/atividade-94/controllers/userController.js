9e762974b10c97845474d98ba3cfd60820cef162
Guilherme Sousa Ribeiro <74858101+GuiK9@users.noreply.github.com>
2022-01-11 22:39:20 -0300

    validate com hapi

--- mais-modulos/atividade-94/controllers/userController.js
+++ mais-modulos/atividade-94/controllers/userController.js
@@ -2,9 +2,15 @@ const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
+const {loginValidate, registerValidate} = require('./validate')
+
 const userController = {
 
     register: async function (req, res) {
+
+        const {error} = registerValidate(req.body)
+        if(error) return res.status(400).send(error.message)
+        
 
         const selectedUser = await User.findOne({email:req.body.email})
 
@@ -25,6 +31,9 @@ const userController = {
     },
 
     login: async function (req, res) {
+
+        const {error} = loginValidate(req.body)
+        if(error) return res.status(400).send(error.message)
 
         const selectedUser = await User.findOne({email:req.body.email})
         if(!selectedUser) return res.status(400).send("email or password incorrect")
