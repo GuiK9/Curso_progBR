4685f333e948b9bb6b9b452e56066333234c5e50
Guilherme Sousa Ribeiro <74858101+GuiK9@users.noreply.github.com>
2022-01-18 17:40:03 -0300

    Hooks

---
+++ mais-modulos/react/atividade-102/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header.js'
+import './App.css';
+import Counter from './Counter.js'
+
+function App() {
+  return(
+    <div>
+    <Counter count={3}></Counter>
+    </div>
+  )
+}
+
+export default App;
+
+//O estado muda depois que a pagina for carregada
+
+//componente: conjunto de elementos que posso criar e fazer uma coisa só, header.js é um componente
---
+++ mais-modulos/react/atividade-102/src/Counter.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+
+
+
+function Counter(props) {
+
+    const [count, setCount] = useState(props.count)
+    const [meuEstado, setMeuEstado] = useState('qualquer coisa')
+
+    function add(){
+
+        setCount(count + 1)
+        console.log('atualizar')
+        console.log(meuEstado)
+    }
+
+    return (
+        <div>
+            <h1>Counter: {count}</h1>
+            <button onClick={add}>add</button>
+        </div>
+    )
+}
+
+/* class Counter extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {count: 0}
+        this.add = this.add.bind(this)
+    }
+
+    add() {
+        //this.setState({count: this.state.count + 1})
+
+        this.setState((state) => { return { count: state.count + 1 } }, () => {
+            console.log(this.state)
+            localStorage.setItem("state", JSON.stringify(this.state))
+        })
+
+
+        /* this.setState((state)=>{return {count: state.count+1}}, () => {
+            console.log(this.state)
+            localStorage.setItem("state", JSON.stringify(this.state))
+        }) 
+    }
+
+    componentDidMount() {
+        this.setState(JSON.parse(localStorage.getItem("state")))
+    }
+
+    componentWillUnmount(){
+        
+    }
+
+    /* shouldComponentUpdate(){
+        return false
+    } 
+
+    render() {
+        return (
+            <div>
+                <h1>Counter: {this.state.count}</h1>
+                <button onClick={this.add}>add</button>
+            </div>)
+    }
+} */
+
+
+export default Counter
