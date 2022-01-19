d5348fb57c4ddb00a64d3e002c21574c1d565454
Guilherme Sousa Ribeiro <74858101+GuiK9@users.noreply.github.com>
2022-01-19 19:01:59 -0300

    To Do list início

---
+++ Projetos/to-do-list/src/Todo.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+function Todo() {
+
+    return (
+        <div className="container">
+            <h1>Todo</h1>
+            <form>
+                <input type="text"></input>
+                <button>Add</button>
+            </form>
+
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+
+    )
+}
+
+
+export default Todo
---
+++ Projetos/to-do-list/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Todo';
+import './ToDo.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
