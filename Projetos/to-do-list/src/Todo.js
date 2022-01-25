f933cf157758e77b5425c3101f2820e8d66d9a5c
Guilherme Sousa Ribeiro <74858101+GuiK9@users.noreply.github.com>
2022-01-25 20:03:48 -0300

    hooks

--- Projetos/to-do-list/src/Todo.js
+++ Projetos/to-do-list/src/Todo.js
@@ -1,23 +1,38 @@
-import React from "react";
-
+import React, { useState } from "react";
+import List from './List'
+import TodoForm from './TodoForm'
+import Item from './Item'
 
 function Todo() {
+
+    const [items, setItems] = useState([])
+
+    function onAddItem(text) {
+        let it = new Item(text)
+        setItems([...items, it])
+
+    }
+
+    function onItemDeleted(item) {
+        let filteredItems = items.filter(it => it.id != item.id)
+        console.log(filteredItems)
+        setItems(filteredItems)
+    }
 
     return (
         <div className="container">
             <h1>Todo</h1>
-            <form>
-                <input type="text"></input>
-                <button>Add</button>
-            </form>
+            <TodoForm onAddItem={onAddItem}></TodoForm>
+            <List onItemDeleted={onItemDeleted} items={items}></List>
 
-            <ul>
-                <li></li>
-            </ul>
         </div>
 
     )
 }
+
+
+
+
 
 
 export default Todo
