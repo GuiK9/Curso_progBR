c4e10c56b56876e89ec0db7be8d9a25eafb8d317
Guilherme Sousa <guilherme.tec1107@gmail.com>
2022-02-04 16:12:01 -0300

    react com redux

---
+++ mais-modulos/react/atividade-106/my-app/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Contador from './componentes/Contador';
+import Cabecalho from './componentes/Cabecalho';
+import {createStore} from 'redux'
+import contadorReducer from './reducers/contadorReducer';
+import { Provider } from 'react-redux'
+
+function App() {
+
+  const store = createStore(contadorReducer)
+
+
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <Cabecalho></Cabecalho>
+        <Contador></Contador>
+      </Provider>
+    </div>
+  )
+}
+
+export default App;
---
+++ mais-modulos/react/atividade-106/my-app/src/componentes/Contador.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { useDispatch, useSelector } from 'react-redux'
+
+
+function Contador(props) {
+
+
+    const contador = useSelector((state) => { return state })
+    const dispatch = useDispatch()
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={() => { dispatch({type:'INCREMENT'}) }}>+</button>
+            <button onClick={() => { dispatch({type:'DECREMENT'}) }}>-</button>
+        </div>
+    )
+}
+
+
+export default Contador
